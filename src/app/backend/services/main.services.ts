b382982ce6386ad3ffc5a3000f17571d00e8a2b7
galexbh <galexbh@protonmail.com>
2022-06-24 22:37:37 -0600

    feat(user, main): create, remove
    A service linked to mongo was created for the creation, search by id and email of users.
    
    Removed the main service

--- src/app/backend/services/main.services.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-
-export class MainService {
-  public welcome(_req: Request, res: Response) {
-    return res.send({ message: "Hello World", });
-  }
-}
--- src/app/backend/services/user.services.ts
+++ src/app/backend/services/user.services.ts
@@ -1,25 +1,13 @@
-import { Request, Response } from "express";
-import { User, UserModel } from '../models/user.models';
+import UserModel, { User } from "../models/user.models";
 
-export class UserServices {
-    public async createUser(req: Request, res: Response) {
-        try {
-            const user: User = await UserModel.create({ ...req.body });
-            return res.status(201).json({ message: "registered", user });
-        } catch (err) {
-            return res.status(400).json({ message: "Unable to register" });
-        }
-    }
+export function createUser(input: Partial<User>) {
+  return UserModel.create(input);
+}
 
-    public async loginUser(req: Request, res: Response) {
-        const { email, password } = req.body;
-        const user: User | null = await UserModel.findOne({ email });
+export function findUserById(id: string) {
+  return UserModel.findById(id);
+}
 
-        if (password !== user?.password) {
-            return res.status(400).json({ message: "not access" });
-        }
-
-        return res.status(201).json({ message: "login", user });
-
-    }
+export function findUserByEmail(email: string) {
+  return UserModel.findOne({ email });
 }
