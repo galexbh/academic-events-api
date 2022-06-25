c208aa28a9f210d731243c33c51c8cdebb9417ee
galexbh <galexbh@protonmail.com>
2022-06-24 22:34:26 -0600

    feat(user): props
    New properties were added to the model, as well as a hashed algorithm for passwords.

--- src/app/backend/models/user.models.ts
+++ src/app/backend/models/user.models.ts
@@ -1,18 +1,62 @@
-import {prop, getModelForClass} from '@typegoose/typegoose';
+import { prop, getModelForClass, DocumentType, pre, modelOptions, Severity, index } from '@typegoose/typegoose';
+import { nanoid } from 'nanoid';
+import argon2 from 'argon2';
+import log from '../shared/logger';
+
+@pre<User>("save", async function () {
+    if (!this.isModified("password")) return;
+
+    const hash = await argon2.hash(this.password);
+
+    this.password = hash;
+
+    return;
+})
+
+@index({ email: 1 })
+
+@modelOptions({
+    schemaOptions: {
+        timestamps: true,
+    },
+    options: {
+        allowMixed: Severity.ALLOW,
+    },
+})
 
 export class User {
 
-    @prop({required: true})
-    public firstname: string;
-
-    @prop({required: true})
-    public lastname: string;
-
-    @prop({required: true, trim: true, lowercase: true, unique: true})
+    @prop({ required: true, trim: true, lowercase: true, unique: true })
     public email: string;
 
-    @prop({required: true, minlength: 8})
+    @prop({ required: true })
+    public firstname: string;
+
+    @prop({ required: true })
+    public lastname: string;
+
+    @prop({ required: true, minlength: 8 })
     public password: string;
+
+    @prop({ required: true, default: () => nanoid() })
+    verificationCode: string;
+
+    @prop()
+    passwordResetCode: string | null;
+
+    @prop({ default: false })
+    verified: boolean;
+
+    async validatePassword(this: DocumentType<User>, candidatePassword: string) {
+        try {
+            return await argon2.verify(this.password, candidatePassword);
+        } catch (e) {
+            log.error(e, "Could not validate password");
+            return false;
+        }
+    }
 }
 
-export const UserModel = getModelForClass(User);
+const UserModel = getModelForClass(User);
+
+export default UserModel;
