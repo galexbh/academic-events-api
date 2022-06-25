d4a0e5f73e42c885106b798fd3d9b48ac7f3d176
galexbh <galexbh@protonmail.com>
2022-06-24 22:25:52 -0600

    feat(mail): nodemailer configuration

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,11 @@
 export default {
     port: "3000",
     logLevel: "info",
+    smtp:{
+        user: 'xuhw3tcckrzobka5@ethereal.email',
+        pass: 'gZMhZt8turxAwr7RgN',
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false,
+    }
 };
