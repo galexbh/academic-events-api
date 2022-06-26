acac6c37ec58612f4188052f606d1685893276b4
galexbh <galexbh@protonmail.com>
2022-06-26 14:35:57 -0600

    feat(roles): initial setup
    role creation at backend startup

--- src/app/backend/libs/initialSetup.ts
+++ src/app/backend/libs/initialSetup.ts
@@ -1,8 +1,21 @@
+import RolModel from "../models/role.model";
+import log from "../shared/logger";
 
+export const createRoles = async () => {
 
-  // roles
-  // const result = await Role.insertMany([
-  //   { name: "admin" },
-  //   { name: "contentCreator" },
-  //   { name: "user" },
-  // ]);
+    try {
+        const count = await RolModel.estimatedDocumentCount();
+
+        if (count > 0) return;
+
+        const result = await RolModel.insertMany([
+            { name: "admin" },
+            { name: "contentCreator" },
+            { name: "user" },
+        ]);
+
+        log.info(result);
+    } catch (e: any) {
+        log.error(e);
+    }
+}
--- src/app/backend/start.ts
+++ src/app/backend/start.ts
@@ -1,8 +1,10 @@
 import { BackendApp } from './backendApp';
+import { createRoles } from './libs/initialSetup';
 import log from './shared/logger';
 
 try {
   new BackendApp().start();
+  createRoles();
 } catch (e) {
   log.error(e);
   process.exit(1);
